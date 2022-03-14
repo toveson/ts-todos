1606e38879d9f53bba8d645d566e3e3f2932243a
Trever O <toveson9522@gmail.com>
2022-03-13 21:51:06 -0600

    created input component for adding todos

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './styles.css'
 
 export const InputField = () => {
   return (
-    <div>
-        Input
-    </div>
+    <form className='input'>
+        <input type='input' placeholder='Enter a task' className='input__box' />
+        <button type='submit' className='input__submit'> GO </button>
+    </form>
   )
 }
