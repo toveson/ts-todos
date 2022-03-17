e821a2588a3d815243b4c239be80d6989a4a70bf
Trever O <toveson9522@gmail.com>
2022-03-16 22:43:00 -0600

    input now excepts text

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import './styles.css'
+import React from "react";
+import "./styles.css";
 
-export const InputField = () => {
-  return (
-    <form className='input'>
-        <input type='input' placeholder='Enter a task' className='input__box' />
-        <button type='submit' className='input__submit'> GO </button>
-    </form>
-  )
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
 }
+
+export const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+  return (
+    <form className="input">
+      <input
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input__box"
+      />
+      <button type="submit" className="input__submit">
+        {" "}
+        GO{" "}
+      </button>
+    </form>
+  );
+};
