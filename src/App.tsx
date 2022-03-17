e7b2ed49f299827e2c448858d834b05d48a3f790
Trever O <toveson9522@gmail.com>
2022-03-16 23:09:58 -0600

    now storing user input locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import "./App.css";
 import { InputField } from "./components/InputField";
+import { Todo } from './model'
 
 const App: React.FC = () => {
     const [todo, setTodo] = useState<string>('');
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const handleAdd = (e: React.FormEvent) => {
+        e.preventDefault()
+
+        if(todo) {
+            setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+            setTodo('');
+        }
+
+    };
 
     console.log(todo)
+    console.log(todos)
   
     return (
     <div className="App">
         <span className="heading">Taskify</span>
-        <InputField todo={todo} setTodo={setTodo} />
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 }
